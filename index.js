30e8701d85c6551757377837ce2c3501833993e1
huongktt7706 <thuyhuong7706nb@gmail.com>
2025-03-13 21:07:46 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let name='Thuy Huong';
+let age='20';
+console.log('Ho va ten:' + name );
+console.log(  age +' '+'Tuoi');
+console.log( name , age  +' '+'Tuoi');
+console.log(`Ho va ten: ${name} ,${age} Tuoi`);
+
+
+let x=15 , y=7;
+let tong=x+y;
+let hieu=x-y;
+let tich=x*y;
+let thuong=x/y;
+let du=x%y;
+console.log(`Phep Tinh: ${tong},${hieu},${tich},${thuong},${du}`)
+
+
+
+
+
+
